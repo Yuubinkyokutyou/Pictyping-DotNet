5842e037426e092326021268b3482d5749ac342a
yuubin <oonnii31@gmail.com>
2025-08-28 18:40:08 +0900

    react config読み込むように修正

--- src/Pictyping.Web/src/main.tsx
+++ src/Pictyping.Web/src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { store } from './store'
 import './index.css'
+import './api/apiConfig' // OpenAPI設定を初期化
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
